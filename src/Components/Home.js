2c2486adefffaa4bb6fcbfc569a56abbfb010624
Jada Woody <JadaWoody@ENCMBP01.local>
2020-09-23 17:12:28 -0400

    wednesday

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,8 +7,10 @@ function Home(props) {
             <nav className="ghost">
                <ul className="spooky">
                    <li className="trick"><Link to={`/NewsPage`}>Get News</Link></li>
+                   <li className="trick"><Link to={`/WeatherPage`}>Get Yo Weather</Link></li>
                    <li className="trick"><Link to={`/SportsPage`}>Sports Updates</Link></li>
                    <li className="trick"><Link to={`/CalmPage`}>Calming Corner</Link></li>
+                   <li className="trick"><Link to={`/HoroscopePage`}>Get Yo Horoscope</Link></li>
                </ul> 
             </nav>
             <body>
