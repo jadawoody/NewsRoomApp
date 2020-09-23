dee526482b4c47bc545e7016f2d5d83aac4e8668
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-23 16:34:00 -0400

    no change

--- src/GetNews.js
+++ src/GetNews.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function GetNews(props) {
-    return (
-        <div>
-            <h3>Get Your Daily Dose of News!</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Get Your Daily Dose of News!</h3>
+    </div>
+  );
 }
 
 export default GetNews;
