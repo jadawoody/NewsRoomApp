44febeab38ff60106f356b29c7b48cb9ca4c60c4
Jada Woody <JadaWoody@ENCMBP01.local>
2020-09-23 17:24:40 -0400

    wednesday

--- src/Components/GetNews.js
+++ src/Components/GetNews.js
@@ -1,34 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../GetNews.css";
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../GetNews.css';
 
 function GetNews(props) {
-  let paper = props.newsDataProp.map((eacharticle) => {
+let paper = props.newsDataProp.map((eacharticle) => {
     // console.log(paper)
-    console.log(eacharticle);
+    console.log(eacharticle)
     return (
-      <div>
-        <ul className="orange">
-          <img
-            className="treat"
-            src={eacharticle.urlToImage}
-            alt="articleImg"
-          />
-          <li className="pumpkin">
-            <a href={eacharticle.url}>{eacharticle.title}</a>
-          </li>
-        </ul>
-      </div>
-    );
-  });
+        <div>
+        
+            <ul className="orange">
+            <img className="treat" src={eacharticle.urlToImage} alt="articleImg"/>
+                <li className="pumpkin">
+                    <a href={eacharticle.url}>{eacharticle.title}</a>
+                </li>
+            </ul>
+            
+        </div>
+    )
+})
 
-  return (
-    <div>
-      <h3>Get Your Daily Dose of News!</h3>
-      {/* <li className="cinnaStick"><Link to={`NewsPage`}>Get News</Link></li> */}
-      <p>{paper}</p>
-    </div>
-  );
+
+    return (
+        <div>
+        
+            <h3>Get Your Daily Dose of News!</h3>
+            {/* <li className="cinnaStick"><Link to={`NewsPage`}>Get News</Link></li> */}
+            <p>{paper}</p>
+            
+        </div>
+    );
 }
 
 //each article has : title, author, description, url
--- src/Components/GetWeather.js
+++ src/Components/GetWeather.js
@@ -3,7 +3,8 @@ import React from 'react';
 function GetWeather(props) {
     return (
         <div>
-            
+            <h3>Welcome To The Weather Page!</h3>
+            <p>Search for Today's Weather by Zipcode!</p>
         </div>
     );
 }
