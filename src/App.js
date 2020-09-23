ecb70e52f81d1dacdf4740977eb8504237460397
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-23 11:57:25 -0400

    niko

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import {Switch, Route} from 'react-router-dom';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h3>Welcome to NewsApp!</h3>
+      <h3>Welcome to NewsApp!!</h3>
     </div>
   );
 }
