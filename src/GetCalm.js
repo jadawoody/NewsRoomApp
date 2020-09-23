f3d89c6edf92e4217a42e7fdeeb4a9d9ab1d31e5
Jada Woody <JadaWoody@ENCMBP01.local>
2020-09-23 11:00:39 -0400

    wednesday

---
+++ src/GetCalm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GetCalm(props) {
+    return (
+        <div>
+            <h3>Calming Corner</h3>
+        </div>
+    );
+}
+
+export default GetCalm;
---
+++ src/GetSports.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GetSports(props) {
+    return (
+        <div>
+            <h3>Catch up on Sports News!</h3>
+        </div>
+    );
+}
+
+export default GetSports;
