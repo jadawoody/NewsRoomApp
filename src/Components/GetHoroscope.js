cdaa4c2bbc1454c50afefd8a74911ce0c46e041a
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-24 14:21:16 -0400

    stuff

--- src/Components/GetHoroscope.js
+++ src/Components/GetHoroscope.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 function GetHoroscope(props) {
-    
-    return (
-        <div>
-            <h3>Your Daily Horoscope</h3>
-
-        </div>
+  let [horoscope, setHoroscope] = useState();
+  const fetchHoroscope = async (e) => {
+    let res = await axios.get(
+      "https://www.prokerala.com/astrology/astrology-api.htm"
     );
+    setHoroscope(res.data);
+    console.log(horoscope);
+  };
+  fetchHoroscope();
+  return (
+    <div>
+      <h3>Your Daily Horoscope</h3>
+    </div>
+  );
 }
 
 export default GetHoroscope;
