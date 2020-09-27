eb0b0be14a873bd1031e3010c7f747ab4c886bf0
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-27 09:59:31 -0400

    rebecca pic

--- src/Components/Creator.js
+++ src/Components/Creator.js
@@ -1,31 +1,58 @@
-import React from 'react';
-import OurLogo from '../images/OURLOGO$.png';
-import {Link} from 'react-router-dom';
-import JadaPic from '../images/headshot jw copy.jpeg'
+import React from "react";
+import OurLogo from "../images/OURLOGO$.png";
+import { Link } from "react-router-dom";
+import JadaPic from "../images/headshot jw copy.jpeg";
+import RebeccaPic from "../images/rebeccaHeadshot.jpg";
 function Creator(props) {
-    return (
-      <div>
-        <header className="logoAllPage">
-        <Link to={`/`}><img src={OurLogo} alt="our news logo" className="ourlogo" ></img></Link>
-        </header>
-        <h4>Meet the Creators</h4>
-        <p>Rebecca Sell and Jada Woody</p>
-        <div className="divOfCreators">
-            <article>
-                <p>Rebecca Sell</p>
-                {/* <img src={RebeccaPic} alt="photoofrebecca" id="rebeccaPic"></img> */}
-                <p id="bio">Cupcake ipsum dolor sit amet cake. Chupa chups cupcake pie toffee gingerbread biscuit carrot cake candy canes. Candy canes tart croissant. Sweet I love sesame snaps. Icing I love sugar plum gummies pudding macaroon tootsie roll. Gummi bears caramels sugar plum pastry gummies biscuit danish. Lollipop topping chocolate bar tootsie roll jelly croissant danish.</p>
-                <a href="https://www.linkedin.com/in/rebecca-sell-/" className="profileLink">Visit Rebecca's LinkedIn Profile</a>
-            </article>
-            <article>
-                <p>Jada Woody</p>
-                <img src={JadaPic} alt="photoofjada" id="jadaPic"></img>
-                <p id="bio">Cupcake ipsum dolor sit amet cake. Chupa chups cupcake pie toffee gingerbread biscuit carrot cake candy canes. Candy canes tart croissant. Sweet I love sesame snaps. Icing I love sugar plum gummies pudding macaroon tootsie roll. Gummi bears caramels sugar plum pastry gummies biscuit danish. Lollipop topping chocolate bar tootsie roll jelly croissant danish.</p>
-                <a href="https://www.linkedin.com/in/jada-woody" className="profileLink">Visit Jada's LinkedIn Profile</a>
-            </article>
-        </div>
+  return (
+    <div>
+      <header className="logoAllPage">
+        <Link to={`/`}>
+          <img src={OurLogo} alt="our news logo" className="ourlogo"></img>
+        </Link>
+      </header>
+      <h4>Meet the Creators</h4>
+      <p>Rebecca Sell and Jada Woody</p>
+      <div className="divOfCreators">
+        <article>
+          <p>Rebecca Sell</p>
+          <img src={RebeccaPic} alt="photoofrebecca" id="rebeccaPic"></img>
+          <p id="bio">
+            Cupcake ipsum dolor sit amet cake. Chupa chups cupcake pie toffee
+            gingerbread biscuit carrot cake candy canes. Candy canes tart
+            croissant. Sweet I love sesame snaps. Icing I love sugar plum
+            gummies pudding macaroon tootsie roll. Gummi bears caramels sugar
+            plum pastry gummies biscuit danish. Lollipop topping chocolate bar
+            tootsie roll jelly croissant danish.
+          </p>
+          <a
+            href="https://www.linkedin.com/in/rebecca-sell-/"
+            className="profileLink"
+          >
+            Visit Rebecca's LinkedIn Profile
+          </a>
+        </article>
+        <article>
+          <p>Jada Woody</p>
+          <img src={JadaPic} alt="photoofjada" id="jadaPic"></img>
+          <p id="bio">
+            Cupcake ipsum dolor sit amet cake. Chupa chups cupcake pie toffee
+            gingerbread biscuit carrot cake candy canes. Candy canes tart
+            croissant. Sweet I love sesame snaps. Icing I love sugar plum
+            gummies pudding macaroon tootsie roll. Gummi bears caramels sugar
+            plum pastry gummies biscuit danish. Lollipop topping chocolate bar
+            tootsie roll jelly croissant danish.
+          </p>
+          <a
+            href="https://www.linkedin.com/in/jada-woody"
+            className="profileLink"
+          >
+            Visit Jada's LinkedIn Profile
+          </a>
+        </article>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Creator;
--- src/Components/GetCalm.js
+++ src/Components/GetCalm.js
@@ -1,40 +1,44 @@
-import React, {useState} from "react";
-import {Link} from 'react-router-dom';
-import OurLogo from '../images/OURLOGO$.png';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import OurLogo from "../images/OURLOGO$.png";
+import axios from "axios";
 
 function GetCalm(props) {
-     console.log(props)
-     let [advice, setAdvice] = useState("");
+  console.log(props);
+  let [advice, setAdvice] = useState("");
 
-     const GetNewAdvice = () => {
-       // console.log(props, props.adviceProp.advice);
-   
-       async function getData() {
-         let res = await axios.get("https://api.adviceslip.com/advice");
-         setAdvice(res.data.slip.advice);
-       }
-       getData();
-   
-       // return <div>{props,adviceProp}</div>;
-     };
-    return (
-        <div>
-        <header className="logoAllPage">
-    <Link to={`/`}><img src={OurLogo} alt="our news logo" className="ourlogo" ></img></Link>
-    </header>
-            <h3>Calming Corner</h3>
-            <p>
+  const GetNewAdvice = () => {
+    // console.log(props, props.adviceProp.advice);
+
+    async function getData() {
+      let res = await axios.get("https://api.adviceslip.com/advice");
+      setAdvice(res.data.slip.advice);
+    }
+    getData();
+
+    // return <div>{props,adviceProp}</div>;
+  };
+  return (
+    <div>
+      <header className="logoAllPage">
+        <Link to={`/`}>
+          <img src={OurLogo} alt="our news logo" className="ourlogo"></img>
+        </Link>
+      </header>
+      <h3>Calming Corner</h3>
+      <div className="insQuote"></div>
+      <p>
         {props.quoteProp}
         <br />-{props.authorProp}
       </p>
       <br />
       <p>{props.adviceProp.advice}</p>
-      <button onClick={GetNewAdvice}></button>
-      <p>{advice}</p>
-            {/* <p>{words}</p> */}
-        </div>
-    );
-
+      <div className="advicebox">
+        <button onClick={GetNewAdvice}></button>
+        <p>{advice}</p>
+        {/* <p>{words}</p> */}
+      </div>
+    </div>
+  );
 }
 export default GetCalm;
