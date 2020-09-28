ccad81d39e712e17c5ec68bff249b18ed0fe0aa0
rqsell <68514875+rqsell@users.noreply.github.com>
2020-09-28 11:16:01 -0400

    broken links

--- src/Components/Creator.js
+++ src/Components/Creator.js
@@ -8,13 +8,25 @@ function Creator(props) {
     <div>
       <header className="logoAllPage">
         <ul className="navheadbar">
-          <li><Link to={`/NewsPage`}>News</Link></li>
-          <li><Link to={`/WeatherPage`}>Weather</Link></li>
-          <li><Link to={`/SportsPage`}>Sports</Link></li>
-          <li><Link to={`/CalmPage`}>Calming Corner</Link></li>
-          <li><Link to={`/TechPage`}>Tech</Link></li>
-          <li><Link to={`/CreatorPage`}>Creators</Link></li>
-          </ul>
+          <li>
+            <Link to={`/NewsPage`}>News</Link>
+          </li>
+          <li>
+            <Link to={`/WeatherPage`}>Weather</Link>
+          </li>
+          <li>
+            <Link to={`/SportsPage`}>Sports</Link>
+          </li>
+          <li>
+            <Link to={`/CalmPage`}>Calming Corner</Link>
+          </li>
+          <li>
+            <Link to={`/TechPage`}>Tech</Link>
+          </li>
+          <li>
+            <Link to={`/CreatorPage`}>Creators</Link>
+          </li>
+        </ul>
         <Link to={`/`}>
           <img src={OurLogo} alt="our news logo" className="ourlogo"></img>
         </Link>
@@ -26,8 +38,13 @@ function Creator(props) {
           <p>Rebecca Sell</p>
           <img src={RebeccaPic} alt="photoofrebecca" id="rebeccaPic"></img>
           <p id="bio">
-            Rebecca Sell is a Full-Stack Developer based in Miami, Florida. As of October 2020, Rebecca has been a graduate alum of Ironhack Miami! Rebecca plans to use her skills learned at Ironhack to launch a career in web development! Rebecca is intersted in projects including the following languages...
-            To get in touch with Rebecca and to learn more about her professional experience, visit her LinkedIn page, linked below! 
+            Rebecca Sell is a Full-Stack Developer based in Miami, Florida. As
+            of October 2020, Rebecca has been a graduate alum of Ironhack Miami!
+            Rebecca plans to use her skills learned at Ironhack to launch a
+            career in web development! Rebecca is intersted in projects
+            including Node.js and React. To get in touch with Rebecca and to
+            learn more about her professional experience, visit her LinkedIn
+            page, linked below!
           </p>
           <a
             href="https://www.linkedin.com/in/rebecca-sell-/"
@@ -40,7 +57,14 @@ function Creator(props) {
           <p>Jada Woody</p>
           <img src={JadaPic} alt="photoofjada" id="jadaPic"></img>
           <p id="bio">
-            Jada Woody is a Full-Stack Developer based in Detroit, Michigan. As of October 2020, she has been a graduate alum of Ironhack Miami. Jada plans to use the skills learned to develop a comprehensive website and software engineering department at Encompass HealthCare and to later launch a career in web development! Jada is interested in working more with Node.js and React. For more information on her experience or to get in touch with Jada, visit her LinkedIn profile, link below!
+            Jada Woody is a Full-Stack Developer based in Detroit, Michigan. As
+            of October 2020, she has been a graduate alum of Ironhack Miami.
+            Jada plans to use the skills learned to develop a comprehensive
+            website and software engineering department at Encompass HealthCare
+            and to later launch a career in web development! Jada is interested
+            in working more with Node.js and React. For more information on her
+            experience or to get in touch with Jada, visit her LinkedIn profile,
+            link below!
           </p>
           <a
             href="https://www.linkedin.com/in/jada-woody"
@@ -51,9 +75,9 @@ function Creator(props) {
         </article>
       </div>
       <footer className="lilFooter">
-          <p>Created by Rebecca Sell and Jada Woody</p>
-          <p>Ironhack Class of Aug 2020</p>
-        </footer>
+        <p>Created by Rebecca Sell and Jada Woody</p>
+        <p>Ironhack Class of Aug 2020</p>
+      </footer>
     </div>
   );
 }
--- src/Components/GetSports.js
+++ src/Components/GetSports.js
@@ -1,13 +1,16 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import OurLogo from '../images/OURLOGO$.png'
+import { Link } from "react-router-dom";
+import OurLogo from "../images/OURLOGO$.png";
 function GetSports(props) {
   let football = props.sportsDataProp.map((eachsport) => {
     return (
-      
       <div>
         <ul className="orange">
-          <img className="treat" src={eachsport.urlToImage} alt="articleImg" />
+          <img
+            className="treat"
+            src={eachsport.urlToImage ? eachsport.urlToImage : OurLogo}
+            alt="articleImg"
+          />
           <li className="pumpkin">
             <a href={eachsport.url}>{eachsport.title}</a>
           </li>
@@ -17,23 +20,37 @@ function GetSports(props) {
   });
   return (
     <div>
-    <header className="logoAllPage">
-    <ul className="navheadbar">
-          <li><Link to={`/NewsPage`}>News</Link></li>
-          <li><Link to={`/WeatherPage`}>Weather</Link></li>
-          <li><Link to={`/SportsPage`}>Sports</Link></li>
-          <li><Link to={`/CalmPage`}>Calming Corner</Link></li>
-          <li><Link to={`/TechPage`}>Tech</Link></li>
-          <li><Link to={`/CreatorPage`}>Creators</Link></li>
-          </ul>
-    <Link to={`/`}><img src={OurLogo} alt="our news logo" className="ourlogo" ></img></Link>
-    </header>
+      <header className="logoAllPage">
+        <ul className="navheadbar">
+          <li>
+            <Link to={`/NewsPage`}>News</Link>
+          </li>
+          <li>
+            <Link to={`/WeatherPage`}>Weather</Link>
+          </li>
+          <li>
+            <Link to={`/SportsPage`}>Sports</Link>
+          </li>
+          <li>
+            <Link to={`/CalmPage`}>Calming Corner</Link>
+          </li>
+          <li>
+            <Link to={`/TechPage`}>Tech</Link>
+          </li>
+          <li>
+            <Link to={`/CreatorPage`}>Creators</Link>
+          </li>
+        </ul>
+        <Link to={`/`}>
+          <img src={OurLogo} alt="our news logo" className="ourlogo"></img>
+        </Link>
+      </header>
       <h3>Catch up on Sports News!</h3>
       {football}
       <footer className="lilFooter">
-          <p>Created by Rebecca Sell and Jada Woody</p>
-          <p>Ironhack Class of Aug 2020</p>
-        </footer>
+        <p>Created by Rebecca Sell and Jada Woody</p>
+        <p>Ironhack Class of Aug 2020</p>
+      </footer>
     </div>
   );
 }
